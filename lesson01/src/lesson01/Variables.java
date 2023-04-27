f71e03f50045e0a3ca286951902b6ffda6083f28
Higa <yh0331tigers@yahoo.co.jp>
2023-04-27 18:29:15 +0900

    演習二日目

---
+++ lesson01/src/lesson01/Variables.java
@@ -0,0 +1,12 @@
+package lesson01;
+
+public class Variables {
+	public static void main(String[] arg) {
+		int a = 12;
+		int b = 13;
+		String str = "こんにちは";
+		String name = "tanaka";
+		String msg = a + b + str + name + a + b;
+		System.out.print(msg);
+	}
+}
