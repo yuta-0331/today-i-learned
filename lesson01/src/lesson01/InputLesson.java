1f8ef007c8d5115597c698386e08651d238b8d5b
Higa <yh0331tigers@yahoo.co.jp>
2023-04-28 19:39:50 +0900

    演習二日目

---
+++ lesson01/src/lesson01/InputLesson.java
@@ -0,0 +1,19 @@
+package lesson01;
+import java.util.Scanner;
+
+public class InputLesson {
+	public static void main(String[] args) {
+		System.out.print("name ?");
+		Scanner inputName = new Scanner(System.in); 
+		String nameStr = inputName.nextLine();
+		
+		System.out.print("age ?");
+		Scanner inputAge = new Scanner(System.in); 
+		String ageStr = inputName.nextLine();
+		int age = Integer.parseInt(ageStr);
+		
+		System.out.println(nameStr + "さんは、80才まであと" + (80 - age) + "年です");
+		
+	}
+
+}
