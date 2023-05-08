d0c82263cf82f4e37b4db1f80e41deac92c48043
Higa <yh0331tigers@yahoo.co.jp>
2023-05-08 18:49:34 +0900

    論理演算子演習

--- GWLesson/GoldenIfStatement04.java
+++ GWLesson/GoldenIfStatement04.java
@@ -10,11 +10,41 @@ public class GoldenIfStatement04 {
 
         System.out.println("生まれ年は？");
         Scanner inputBirthYearScanner = new Scanner(System.in);
-        int birthYear = inputBirthYearScanner.nextInt();
+        int birthYear;
+        try {
+            birthYear = inputBirthYearScanner.nextInt();
+        } catch (NumberFormatException e) {
+            System.out.println("数値で入力してください");
+            inputBirthYearScanner.close();
+            inputNameScanner.close();
+            return;
+        }
+
+        System.out.println("男性なら1を、女性なら2を入力してください");
+        Scanner inputSexNumScanner = new Scanner(System.in);
+        int inputSexNum;
+        try {
+            inputSexNum = inputBirthYearScanner.nextInt();
+            if (inputSexNum != 1 && inputSexNum != 2) {
+                System.out.println("1か2を入力してください");
+                inputNameScanner.close();
+                inputBirthYearScanner.close();
+                inputSexNumScanner.close();
+                return;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("数値で入力してください");
+            inputNameScanner.close();
+            inputBirthYearScanner.close();
+            inputSexNumScanner.close();
+            return;
+        }
 
         String generation;
 
-        if (birthYear <= 1969) {
+        if (inputSexNum == 2) {
+            generation = "いつまでもシンデレラ";
+        } else if (birthYear <= 1969) {
             generation = "ベビーブーマー世代";
         } else if (birthYear <= 1979) {
             generation = "氷河期世代";
@@ -23,12 +53,13 @@ public class GoldenIfStatement04 {
         } else {
             generation = "Z世代";
         }
-
+        
         System.out.println(
             inputName + "さんは" + generation + "です。"
         );
 
         inputBirthYearScanner.close();
         inputNameScanner.close();
+        inputSexNumScanner.close();
     }
 }
