90f66cca59023303fed76f3093032afbaf1842f5
Higa <yh0331tigers@yahoo.co.jp>
2023-05-21 16:46:45 +0900

    引数名を変更

--- BingoGame2.java
+++ BingoGame2.java
@@ -6,27 +6,27 @@ public class BingoGame2 {
 
     //引数のindex要素に、重複なしで番号を格納するメソッド(盤面の作成、当たり番号の抽選で使用)
     //range: 取り得る値の範囲, adjust: 取り得る値の調整,
-    public static int[] generateArrayWithUniqueNumber(int index, int[] args, int range, int adjust) {
+    public static int[] generateArrayWithUniqueNumber(int index, int[] array, int range, int adjust) {
         while (true) {
             int tmp = (int) (Math.random() * range) + adjust;
             boolean continueFlag = false;
             for (int i = 0; i < index; i++) {
-                if (args[i] == tmp) {
+                if (array[i] == tmp) {
                     continueFlag = true;
                 }
             }
             if (continueFlag) {
                 continue;
             } else {
-                args[index] = tmp;
+                array[index] = tmp;
                 break;
             }
         }
-        return args;
+        return array;
     }
 
     //盤面の行配列を引数に格納するメソッド
-    public static int[][] createBoard(int[][] args) {
+    public static int[][] createBoard(int[][] boardArray) {
         //縦の列配列を作成
         int[][] columnNumbers = new int [5][5];
         for (int i = 0, j = 1; i < 5; i++, j += 15) {
@@ -37,14 +37,14 @@ public class BingoGame2 {
         //縦の列配列を引数に格納
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                args[i][j] = columnNumbers[j][i];
+                boardArray[i][j] = columnNumbers[j][i];
             }
         }
         return boardNumbers;
     }
 
     //引数に盤面の配列を渡すと、盤面を出力するメソッド
-    public static void printBoard(int[][] args) {
+    public static void printBoard(int[][] boardArray) {
         System.out.println("---------------------");
         System.out.println("| B | I | N | G | O |");
         System.out.println("---------------------");
@@ -52,8 +52,8 @@ public class BingoGame2 {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 String temp;
-                if (args[i][j] > 0) {
-                    temp = args[i][j] < 10 ? ("| " + args[i][j] + " ") : ("| " + args[i][j]);
+                if (boardArray[i][j] > 0) {
+                    temp = boardArray[i][j] < 10 ? ("| " + boardArray[i][j] + " ") : ("| " + boardArray[i][j]);
                 } else {
                     temp = "| ■ ";
                 }
@@ -64,12 +64,12 @@ public class BingoGame2 {
     }
 
     //引数の要素にnumを持っていてたら、値を0に置換するメソッド
-    public static void checkHitNumber(int num, int[][] args) {
+    public static void checkHitNumber(int hitNum, int[][] boardArray) {
         boolean hitFlag = false;
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                if (args[i][j] == num) {
-                    args[i][j] = 0;
+                if (boardArray[i][j] == hitNum) {
+                    boardArray[i][j] = 0;
                     hitFlag = true;
                     break;
                 }
@@ -77,20 +77,21 @@ public class BingoGame2 {
         }
         System.out.println(hitFlag ? "Hit!" : "Deviate");
     }
+    
     //引数に渡した盤面の配列がクリア条件を満たしていないかチェック
-    public static boolean checkAlign(int[][] args) {
+    public static boolean checkAlign(int[][] boardArray) {
         boolean alignFlag = false;
         for (int i = 0; i < 5; i++) {
             if (
-                args[i][0] == 0 && args[i][1] == 0 && args[i][2] == 0 && args[i][3] == 0 && args[i][4] == 0 ||
-                args[0][i] == 0 && args[1][i] == 0 && args[2][i] == 0 && args[3][i] == 0 && args[4][i] == 0
+                boardArray[i][0] == 0 && boardArray[i][1] == 0 && boardArray[i][2] == 0 && boardArray[i][3] == 0 && boardArray[i][4] == 0 ||
+                boardArray[0][i] == 0 && boardArray[1][i] == 0 && boardArray[2][i] == 0 && boardArray[3][i] == 0 && boardArray[4][i] == 0
             ) {
                     alignFlag = true;
                     break;
             }
             if (
-                args[0][0] == 0 && args[1][1] == 0  && args[3][3] == 0 && args[4][4] == 0 ||
-                args[0][4] == 0 && args[1][3] == 0 && args[3][1] == 0 && args[4][0] == 0
+                boardArray[0][0] == 0 && boardArray[1][1] == 0  && boardArray[3][3] == 0 && boardArray[4][4] == 0 ||
+                boardArray[0][4] == 0 && boardArray[1][3] == 0 && boardArray[3][1] == 0 && boardArray[4][0] == 0
             ) {
                     alignFlag = true;
                     break;
