4e38fcf6416d5b53afd8649facc48206f20e35dc
Higa <yh0331tigers@yahoo.co.jp>
2023-05-22 22:05:59 +0900

    create BubbleSort class

---
+++ src/lesson/BubbleSort.java
@@ -0,0 +1,25 @@
+package lesson;
+
+public class BubbleSort {
+
+	public static void doSort(int[] args) {
+		for (int i = 0; i < args.length - 1; i++) {
+			for (int j = 0; j < args.length - 1 - i; j++) {
+				if (args[j] > args[j + 1]) {
+					int temp = args[j];
+					args[j] =  args[j + 1];
+					args[j + 1] = temp;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] numbers = { 3, 4, 5, 6, 2, 1 };
+		doSort(numbers);
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.println(numbers[i]);
+		}
+	}
+
+}
