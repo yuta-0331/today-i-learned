b5007779ed332f919f2e83c84eb1b2406dd6e8e0
Higa <yh0331tigers@yahoo.co.jp>
2023-05-22 18:01:48 +0900

    create forAndArray

--- src/lesson/MyConsole.java
+++ src/lesson/MyConsole.java
@@ -1,5 +1,22 @@
 package lesson;
 
-public class MyConsole {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class MyConsole {
+    public static String readLine() {
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        String str = null;
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return str;
+    }
 }
+
