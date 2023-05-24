ef9ff0cc5e88bcc54dfb7e6ea9fe1c1b17684bd4
Higa <yh0331tigers@yahoo.co.jp>
2023-05-24 17:34:29 +0900

    fixed TopScorerCalculator

--- src/lesson/TopScorerCalculator.java
+++ src/lesson/TopScorerCalculator.java
@@ -8,18 +8,27 @@ public class TopScorerCalculator {
 				"田中", "斉藤", "大城", "皆戸", "神藤"
 		};
 		int[] scores = { 86, 72, 94, 33, 94 };
-		
+
 		int index = 0;
+		int count = 1;
 		for (int i = 1; i < names.length; i++) {
 			if (scores[i] > scores[index]) {
 				index = i;
+				count = 1;
+			} else if (scores[i] == scores[index]) {
+				count++;
 			}
 		}
-		System.out.print("最高得点者:");
-		for (int i = 0; i < names.length; i++) {
+		int[] topScorerIndexs = new int[count];
+		for (int i = 0, j = 0; i < names.length && j < count; i++) {
 			if (scores[i] == scores[index]) {
-				System.out.print(names[i] + " ");
+				topScorerIndexs[j++] = i;
 			}
+		}
+		
+		System.out.print("最高得点者:");
+		for (int i = 0; i < count; i++) {
+			System.out.print(names[topScorerIndexs[i]] + " ");
 		}
 		System.out.print("最高得点:" + scores[index]);
 	}
