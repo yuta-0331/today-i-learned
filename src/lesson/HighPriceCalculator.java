a7496c6e4eae313e90305d0aaa8c8937ae91855c
Higa <yh0331tigers@yahoo.co.jp>
2023-06-01 19:38:24 +0900

    fixed HighPriceCalculator

--- src/lesson/HighPriceCalculator.java
+++ src/lesson/HighPriceCalculator.java
@@ -12,6 +12,25 @@ public class HighPriceCalculator {
 		return highPriceBook;
 	}
 
+	public static int countSamePrice(Book[] args , int price) {
+		int count = 0;
+		for (Book book : args) {
+			if (book.price == price) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static Book[] createArraySamePrice(Book[] args, int price, int count) {
+		Book[] bookArray = new Book[count];
+		for (int i = 0, j = 0; i < args.length && j < count; i++) {
+			if (args[i].price == price) {
+				bookArray[j++] = args[i];
+			}
+		}
+
+	}
 	public static void main(String[] args) {
 
 		Book[] books = new Book[5];
@@ -27,7 +46,13 @@ public class HighPriceCalculator {
 		}
 		
 		Book highPriceBook = searchHighPriceBook(books);
-		System.out.println(highPriceBook.title + "\t" + highPriceBook.price + "円です");
+		int count = countSamePrice(books, highPriceBook.price);
+		Book[] highPriceBooks = createArraySamePrice(books, highPriceBook.price, count);
+		System.out.println("最高価格の本")
+		for (Book book : highPriceBooks) {
+			System.out.print(book.title + " ");
+		}
+		System.out.println("価格は" + highPriceBook.price);
 		
 	}
 
