a143fbe2ed62e8303e1a2ac3894ab2f7651ba810
Higa <yh0331tigers@yahoo.co.jp>
2023-06-10 19:30:31 +0900

    create class HelloServlet

--- src/lesson/HelloServlet.java
+++ src/lesson/HelloServlet.java
@@ -1,2 +1,14 @@
-package lesson;public class HelloServlet {
+package lesson;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+    }
 }
