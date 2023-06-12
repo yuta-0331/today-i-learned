431f0bfe9931700429a1f5556fa0ea990ecb27f0
Higa <yh0331tigers@yahoo.co.jp>
2023-06-12 21:00:24 +0900

    fixed doGet method

--- src/lesson/HelloServlet.java
+++ src/lesson/HelloServlet.java
@@ -1,14 +1,22 @@
 package lesson;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-
+    protected void doGet(
+            HttpServletRequest request, HttpServletResponse response
+    ) throws ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("...");
+        out.println("</html>");
     }
 }
