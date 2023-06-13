c384399facbd809b1546fa16f69c599c3768497a
Higa <yh0331tigers@yahoo.co.jp>
2023-06-13 18:00:48 +0900

    use ArrayList

--- src/lesson/Book.java
+++ src/lesson/Book.java
@@ -4,4 +4,9 @@ public class Book {
 	public int price;
 	public String title;
 
+	public Book(String title, int price) {
+		this.title = title;
+		this.price = price;
+	}
+
 }
--- src/lesson/HighPriceCalculator.java
+++ src/lesson/HighPriceCalculator.java
@@ -1,9 +1,11 @@
 package lesson;
 
+import java.util.ArrayList;
+
 public class HighPriceCalculator {
 	
-	public static Book searchHighPriceBook(Book[] args) {
-		Book highPriceBook = args[0];
+	public static Book searchHighPriceBook(ArrayList<Book> args) {
+		Book highPriceBook = args.get(0);
 		for (Book book : args) {
 			if (book.price > highPriceBook.price) {
 				highPriceBook = book;
@@ -12,42 +14,26 @@ public class HighPriceCalculator {
 		return highPriceBook;
 	}
 
-	public static int countSamePrice(Book[] args , int price) {
-		int count = 0;
-		for (Book book : args) {
-			if (book.price == price) {
-				count++;
-			}
-		}
-		return count;
-	}
-
-	public static Book[] createArraySamePrice(Book[] args, int price, int count) {
-		Book[] bookArray = new Book[count];
-		for (int i = 0, j = 0; i < args.length && j < count; i++) {
-			if (args[i].price == price) {
-				bookArray[j++] = args[i];
+	public static ArrayList<Book> createArraySamePrice(ArrayList<Book> args, int price) {
+		ArrayList<Book> bookArray = new ArrayList<>();
+		for (int i = 0, j = 0; i < args.size(); i++) {
+			if (args.get(i).price == price) {
+				bookArray.add(args.get(i));
 			}
 		}
 		return bookArray;
 	}
 	public static void main(String[] args) {
-
-		Book[] books = new Book[5];
-		for (int i = 0; i < books.length; i++) {
-			books[i] = new Book();
-		}
+		ArrayList<Book> books = new ArrayList<>();
 		
 		String[] titles = { "初めてのWebGL", "PHP本格入門", "SQL入門", "JavaScript新版", "Java基本編" };
-		int[] prices = { 4200, 3800, 2980, 3200, 1600 };
-		for (int i = 0; i < books.length; i++) {
-			books[i].title = titles[i];
-			books[i].price = prices[i]; 
+		int[] prices = { 4200, 3800, 4200, 3200, 1600 };
+		for (int i = 0; i < titles.length; i++) {
+			books.add(new Book(titles[i], prices[i]));
 		}
 		
 		Book highPriceBook = searchHighPriceBook(books);
-		int count = countSamePrice(books, highPriceBook.price);
-		Book[] highPriceBooks = createArraySamePrice(books, highPriceBook.price, count);
+		ArrayList<Book> highPriceBooks = createArraySamePrice(books, highPriceBook.price);
 		System.out.println("最高価格の本");
 		for (Book book : highPriceBooks) {
 			System.out.print(book.title + " ");
