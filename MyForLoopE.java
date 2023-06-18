73b2061b6d61732a025a7be9b37dbe4d5bc34bf4
Higa <yh0331tigers@yahoo.co.jp>
2023-06-18 16:03:01 +0900

    file整理

--- MyForLoopE.java
+++
@@ -1,9 +0,0 @@
-public class MyForLoopE {
-    public static void main(String[] args) {
-        int total = 0;
-        for (int i = 0; i <= 100; i++) {
-            total += i;
-        }
-        System.out.println("1~100の和の合計は" + total + "です");
-    }
-}
