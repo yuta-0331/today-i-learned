8b516fb3a8e9f9dd5cc65011e0da9a8d77186668
Higa <yh0331tigers@yahoo.co.jp>
2023-06-29 21:56:04 +0900

    practice SQL

--- src/practiceSQL.sql
+++ src/practiceSQL.sql
@@ -1,4 +1,19 @@
-CREATE TABLE 従業員(
-    ID int IDENTITY(1, 1) CONSTRAINT PK_従業員 PRIMARY KEY (ID),
-
+use sampleDB01
+CREATE TABLE 部門(
+    部門ID int IDENTITY(1, 1) CONSTRAINT PK_所属 PRIMARY KEY (部門ID),
+    部門 nvarchar(30) NOT NULL
 )
+INSERT INTO 部門(部門) VALUES (N'営業')
+INSERT INTO 部門 VALUES (N'エンジニア')
+
+SELECT * FROM 部門
+
+CREATE TABLE 社員(
+    ID int IDENTITY(1, 1) CONSTRAINT PK_社員 PRIMARY KEY (ID),
+    姓 nvarchar(30) NOT NULL,
+    名 nvarchar(30) NOT NULL,
+    部門ID int NOT NULL CONSTRAINT FK_部門ID FOREIGN KEY (部門ID) REFERENCES 部門(部門ID)
+)
+INSERT INTO 社員 VALUES (N'田中', N'正俊', 3)
+
+SELECT * FROM 社員
