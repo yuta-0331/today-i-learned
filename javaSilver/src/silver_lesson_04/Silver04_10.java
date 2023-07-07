01e77e1d8130b372f7ddcb31f12f2c0d6bf8829a
Higa <yh0331tigers@yahoo.co.jp>
2023-07-07 22:35:21 +0900

    learn java silver 4

---
+++ javaSilver/src/silver_lesson_04/Silver04_10.java
@@ -0,0 +1,16 @@
+package silver_lesson_04;
+
+public class Silver04_10 {
+    public static void main(String[] args) {
+       for (int i = 0; true; i++) {
+           System.out.println(i);
+       }
+       for (int i = 0; ; i++) {
+           System.out.println(i);
+       }
+       for (int i = 0; i < 5;) {
+           System.out.println(i);
+       }
+    }
+
+}
