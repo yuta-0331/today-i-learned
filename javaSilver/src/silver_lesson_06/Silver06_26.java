de4e17b12bd064c0db58007a4c450b4b25929a9f
Higa <yh0331tigers@yahoo.co.jp>
2023-07-09 15:02:18 +0900

    learn java silver 6(instance & method)

---
+++ javaSilver/src/silver_lesson_06/Silver06_26.java
@@ -0,0 +1,10 @@
+package silver_lesson_06;
+
+class Parent {
+    int num = 10;
+}
+public class Silver06_26 extends Parent {
+    public static void main(String[] args) {
+//        System.out.println(num);
+    }
+}
