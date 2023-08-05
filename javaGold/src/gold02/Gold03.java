2ca4002dad2965939d8f80f13db4ced9748f9105
Higa <yh0331tigers@yahoo.co.jp>
2023-08-05 22:37:24 +0900

    builder pattern

--- javaGold/src/gold02/Gold03.java
+++ javaGold/src/gold02/Gold03.java
@@ -23,7 +23,7 @@ class Item03 {
         return "Item [id=" + id + ", name=" + name + ", price=" + price + "]";
     }
 
-    static class Builder {
+    public static class Builder {
         private int id;
         private String name;
         private int price;
@@ -43,13 +43,57 @@ class Item03 {
         }
     }
 }
-
+// インスタンス生成時に、どのフィールドにどの値をセットするのかわかりやすい
 class Sample03 {
     public static void main(String[] args) {
         Item03 item = new Item03.Builder(100)
                 .setName("orange")
                 .setPrice(120)
                 .build();
+        System.out.println(item);
+
+    }
+}
+
+class Item03kai {
+    private int id;
+    private String name;
+    private int price;
+
+    private Item03kai(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.price = builder.price;
+    }
+    @Override
+    public String toString() {
+        return "Item [id=" + id + ", name=" + name + ", price=" + price + "]";
+    }
+
+    public static class Builder {
+        private int id;
+        public String name;
+        public int price;
+
+        public Builder(int id) {
+            this.id = id;
+        }
+        public Builder with(Consumer<Builder> consumer) {
+            consumer.accept(this);
+            return this;
+        }
+        public Item03kai build() {
+            return new Item03kai(this);
+        }
+    }
+}
+class Sample03kai {
+    public static void main(String[] args) {
+        Item03kai item = new Item03kai.Builder(100)
+                .with(b -> {
+                    b.name = "orange";
+                    b.price = 120;
+                }).build();
         System.out.println(item);
     }
 }
