cdd8c4c696b0c81986d73311a49c128b3f61dc23
Higa <yh0331tigers@yahoo.co.jp>
2023-08-22 21:07:10 +0900

    create ger

--- javaGold/src/gold02/Gold03.java
+++ javaGold/src/gold02/Gold03.java
@@ -27,6 +27,19 @@ class Item03 {
         private int id;
         private String name;
         private int price;
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+
         public Builder(int id) {
             this.id = id;
         }
