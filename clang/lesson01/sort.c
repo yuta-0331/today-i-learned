2e596c3a4ee79ea810bd68e7f539366328e21224
Higa <yh0331tigers@yahoo.co.jp>
2023-09-05 19:14:25 +0900

    create sort program

---
+++ clang/lesson01/sort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    // 配列の作成
+    int numbers[10];
+    for (int i = 0; i < 10; i++) {
+        int inputNum;
+        printf("数値を入力してください\n");
+        scanf("%d", &inputNum);
+        numbers[i] = inputNum;
+    }
+
+    // sortのアルゴリズム
+    for (int i = 0, j = 9; i < 10; i++, j--) {
+        int maxIndex = 0;
+        for (int k = 1; k < 10 - i; k++) {
+            if (numbers[k] > numbers[maxIndex]) {
+                maxIndex = k;
+            }
+        }
+        int tmp = numbers[maxIndex];
+        numbers[maxIndex] = numbers[j];
+        numbers[j] = tmp;
+    }
+
+    // 配列の表示
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", numbers[i]);
+    }
+}
